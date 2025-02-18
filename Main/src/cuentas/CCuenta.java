e46ddb2a83236fa0e31a6edcb092af9f2bdd3a24
berto <bertouther@hotmail.com>
2025-02-18 10:42:33 +0100

    segundo commit NetBeans

--- Main/src/cuentas/CCuenta.java
+++ Main/src/cuentas/CCuenta.java
@@ -1,58 +1,55 @@
 package cuentas;
 
+/**
+ * @author Alberto Calvo Pazos
+ */
+
 public class CCuenta {
 
     /**
-     * @return the nombre
+     * @return nombre del titular de la cuenta
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * @param nombre nombre del titular de la cuenta a crear
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * @return numero de cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * @param cuenta numero de cuenta a crear
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
-     */
-    public double getSaldo() {
-        return saldo;
-    }
-
-    /**
-     * @param saldo the saldo to set
+     * @param saldo saldo a configurar en al cuenta
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * @return el tipo de interés de la cuenta
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * @param tipoInterés configura el tipo de interes de la cuenta
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
@@ -64,35 +61,61 @@ public class CCuenta {
     private double saldo;
     private double tipoInterés;
 
+    
+    /**
+     * 
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * 
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+    
+    
+    /**
+     * 
+     * @return saldo de la cuenta
+     */
     public double estado()
     {
-        return getSaldo();
+        return saldo;
     }
 
+    /**
+     * 
+     * @param cantidad
+     * @throws Exception 
+     */
     public void ingresar(double cantidad) throws Exception
-    {
+    {   
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        setSaldo(getSaldo() + cantidad);
+        setSaldo(saldo + cantidad);
     }
-
+    
+    /**
+     * 
+     * @param cantidad
+     * @throws Exception 
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        setSaldo(getSaldo() - cantidad);
+        setSaldo(saldo - cantidad);
     }
 }
